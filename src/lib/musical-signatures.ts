51b8875453efa1420c45c7ed48742f15c055e195
Philip Saa <cowglow@gmail.com>
2020-06-15 09:34:24 +0200

    Updates corresponding sharps in major scale
    Signed-off-by: Philip Saa <cowglow@gmail.com>

--- src/lib/musical-signatures.ts
+++ src/lib/musical-signatures.ts
@@ -1,18 +1,23 @@
 type scale = string[];
 
-const majorC: scale = ["C", "D", "E", "F", "G", "A", "B"];
+/**
+ * MAJOR KEY STRUCTURE
+ * WHOLE STEP - WHOLE STEP - HALF STEP - WHOLE STEP - WHOLE STEP - WHOLE STEP - HALF STEP
+ *
+ * CIRCLE OF FIFTHS
+ * C D E F G A B
+ */
 
-const majorG: scale = ["C", "D", "E", "F", "G#", "A", "B"]; // 1 #
+// 7 steps for the sharps
+const majorC: scale = ["C",  "D",  "E",  "F",  "G",  "A",  "B"]; // No Sharps
+const majorG: scale = ["C",  "D",  "E",  "F#", "G",  "A",  "B"]; // 1 - F#
+const majorD: scale = ["C#", "D",  "E",  "F#", "G",  "A",  "B"]; // 2 - F# C#
+const majorA: scale = ["C#", "D",  "E",  "F#", "G#", "A",  "B"]; // 3 - F# C# G#
+const majorE: scale = ["C#", "D#", "E",  "F#", "G#", "A",  "B"]; // 4 - F# C# G# D#
+const majorB: scale = ["C#", "D#", "E",  "F#", "G#", "A#", "B"]; // 5 - F# C# G# D# A#
+const majorF: scale = ["C#", "D#", "E#", "F#", "G#", "A#", "B"]; // 6 - F# C# G# D# A# E#
 
-const majorD: scale = ["C#", "D", "E", "F", "G#", "A", "B"]; // 2 #
-
-const majorA: scale = ["C#", "D", "E", "F#", "G#", "A", "B"]; // 3 #
-
-const majorE: scale = ["C#", "D#", "E", "F#", "G#", "A", "B"]; // 4 #
-
-const majorB: scale = ["C#", "D#", "E", "F#", "G#", "A#", "B"]; // 5 #
-
-const majorF: scale = ["C#", "D#", "E#", "F#", "G#", "A#", "B#"]; // #6
+// 4 for the flats
 
 interface musicalSignatures {
   label: string;
@@ -28,24 +33,3 @@ export const majorKeys: musicalSignatures[] = [
   { label: "major A", keys: majorA },
   { label: "major B", keys: majorB }
 ];
-
-/**
- * MAJOR KEY STRUCTURE
- *
- * C
- * WHOLE STEP
- * D
- * WHOLE STEP
- * E
- * HALF STEP
- * F
- * WHOLE STEP
- * G
- * WHOLE STEP
- * A
- * WHOLE STEP
- * B
- * HALF STEP
- * C
- *
- */
